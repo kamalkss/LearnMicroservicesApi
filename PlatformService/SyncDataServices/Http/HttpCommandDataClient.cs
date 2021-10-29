f9b1cf72a05059f90b6a2984e243f9b767752121
kamal kamel <kamalkss@hotmail.com>
2021-10-29 10:47:45 +0330

    httpasync

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,30 @@
+﻿using PlatformService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpCommandDataClient(HttpClient httpClient )
+        {
+            _httpClient = httpClient;
+        }
+        public async Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            var httpContetn = new StringContent(
+                JsonSerializer.Serialize(plat),
+                Encoding.UTF8,
+                "application/json"
+                );
+            var response = await _httpClient.PostAsync("",httpContetn);
+
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("This is success");
+            }
+        }
+    }
+}
